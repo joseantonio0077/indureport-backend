5c955171998842d70971d95e844c559a0462400d
joseantonio0077 <engine.cancino@gmail.com>
2025-05-15 14:43:42 -0400

    Add files via upload

---
+++ utils/responseHandler.js
@@ -0,0 +1,10 @@
+// utils/responseHandler.js
+const sendResponse = (res, status, success, message, data = null) => {
+  return res.status(status).json({
+    success,
+    message,
+    data
+  });
+};
+
+module.exports = { sendResponse };

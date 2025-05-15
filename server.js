e15757c906a533b8d3d498c09097eca843f0440d
joseantonio0077 <engine.cancino@gmail.com>
2025-05-15 14:39:52 -0400

    Update server.js
    // En server.js
    const helmet = require('helmet');
    const rateLimit = require('express-rate-limit');
    
    // Middleware
    app.use(helmet());
    app.use('/api/', rateLimit({
      windowMs: 15 * 60 * 1000, // 15 minutos
      max: 100 // límite por IP
    }));

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const connectDB = require('./config/db');
 require('dotenv').config();
 
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 // Conectar a la base de datos
 connectDB();
 
@@ -16,6 +18,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use(helmet());
+app.use('/api/', rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100 // límite por IP
+}));
 
 // Servir archivos estáticos (imágenes)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
